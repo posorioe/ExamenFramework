04683ee5559a829e0338517a0a9a05e54ec2962c
paaosori <paaosori@bancolombia.com.co>
2018-12-16 20:16:02 -0500

    Proyecto Web - Inicio

---
+++ Web/AutomatizacionWeb/Object Repository/Tipo_Persona.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Tipo_Persona</name>
+   <tag></tag>
+   <elementGuidId>14a378c3-7a75-4b2e-b6a4-e28094d05d65</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>s2id_autogen16_search</value>
+   </webElementProperties>
+</WebElementEntity>
